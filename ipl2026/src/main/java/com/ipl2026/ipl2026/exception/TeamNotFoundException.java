d9df9937d40280a4fa141659d2616254a69baaf5
Abhiram <abhiram.b@icloud.com>
2026-05-19 18:53:05 +0530

    Delete ipl2026 directory

--- ipl2026/src/main/java/com/ipl2026/ipl2026/exception/TeamNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ipl2026.ipl2026.exception;
-
-public class TeamNotFoundException extends RuntimeException {
-    public TeamNotFoundException(String message) {
-        super(message);
-    }
-}
