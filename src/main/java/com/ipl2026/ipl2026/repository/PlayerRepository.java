e6f3ae025d7a555e93a0586cc76c05840055127b
Abhiram <abhiram.b@icloud.com>
2026-06-02 09:56:36 +0530

    Delete src directory

--- src/main/java/com/ipl2026/ipl2026/repository/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ipl2026.ipl2026.repository;
-
-import com.ipl2026.ipl2026.entity.Players;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Players, Long> {
-}
--- src/main/java/com/ipl2026/ipl2026/service/PlayerService.java
+++
@@ -1,8 +0,0 @@
-package com.ipl2026.ipl2026.service;
-
-import com.ipl2026.ipl2026.dto.Request.PlayersRequest;
-
-public interface PlayerService {
-
-    String create(PlayersRequest playersRequest);
-}
